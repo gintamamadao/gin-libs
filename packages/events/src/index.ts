4eb5da3ea54d0c298e9c907e3738c290763d72b9
gintamamadao <gintamaginsan@126.com>
2021-03-14 16:35:05 +0800

    feat: events util

---
+++ packages/events/src/index.ts
@@ -0,0 +1,53 @@
+import { isFunc } from 'ginlibs-type-check'
+
+class Events {
+  private eventsMap: any = {}
+  private globalConfig: any = {}
+  constructor(config?: any) {
+    this.globalConfig = config || {}
+  }
+
+  on = (taskName: string, handle: Function) => {
+    const eventsMap = this.eventsMap
+    const handleList = eventsMap[taskName] || []
+    if (isFunc(handle)) {
+      handleList.push(handle)
+    }
+    eventsMap[taskName] = handleList
+    this.eventsMap = eventsMap
+    return () => {
+      this.off(taskName, handle)
+    }
+  }
+
+  emit = async (taskName: string, ...arg: any[]): Promise<any> => {
+    const eventsMap = this.eventsMap
+    const handleList = eventsMap[taskName] || []
+    const res = await Promise.all(
+      handleList.map((handle) =>
+        handle.apply(null, [...arg, this.globalConfig])
+      )
+    )
+
+    if (res.length <= 1) {
+      return res[0]
+    } else {
+      return res
+    }
+  }
+
+  off = (taskName: string, handle?: Function) => {
+    const eventsMap = this.eventsMap || {}
+    let handleList = eventsMap[taskName] || []
+    if (isFunc(handle)) {
+      eventsMap[taskName] = []
+    } else {
+      handleList = handleList.filter((eventFn) => {
+        return eventFn !== handle
+      })
+      eventsMap[taskName] = handleList
+    }
+  }
+}
+
+export default Events
