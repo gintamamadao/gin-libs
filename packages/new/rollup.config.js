f66d1de99a113d1b9b9ec18335ccb4d7abc2ba64
yuan.li <yuan.li@shopee.com>
2021-03-21 14:17:40 +0800

    feat: new lib

--- packages/new/rollup.config.js
+++ packages/new/rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
 import pkg from './package.json'
+import shelljs from 'shelljs'
 
 const extensions = ['.js', '.ts']
 
@@ -20,6 +21,18 @@ export default [
     input: 'src/index.ts',
     external: Object.keys(pkg.dependencies || {}),
     output: [{ file: pkg.main, format: 'cjs' }],
-    plugins: [json(), typescript(), resolve(), commonjs(), babel(babelOpt)],
+    plugins: [
+      json(),
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel(babelOpt),
+      {
+        name: 'cpTplFile',
+        writeBundle() {
+          shelljs.cp('-R', 'src/tplFile', 'lib/')
+        },
+      },
+    ],
   },
 ]
