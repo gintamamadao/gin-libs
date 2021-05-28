3b9a4b7a516a90d7dbe17e9f47f31c126ff7bacb
gintamamadao <gintamaginsan@126.com>
2021-05-29 02:46:58 +0800

    feat: md note

--- packages/md-notes/src/functions/mdTreeShaking.ts
+++ packages/md-notes/src/functions/mdTreeShaking.ts
@@ -2,10 +2,14 @@ import { addChildAndParent } from './addChildAndParent'
 import { mdPretty } from './mdPretty'
 import { delNotExistParent } from './delNotExistParent'
 import { delNotExistChild } from './delNotExistChild'
+import { setNoteTitle } from './setNoteTitle'
+import { setParentName } from './setParentName'
 
 export const mdTreeShaking = async () => {
   delNotExistChild()
   delNotExistParent()
   addChildAndParent()
+  setParentName()
+  setNoteTitle()
   mdPretty()
 }
